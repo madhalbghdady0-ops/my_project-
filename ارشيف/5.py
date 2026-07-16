eab7961caacc811d9b7c00908a3e18e9271f628a
Moaz <madhalbghdady0@gmail.com>
2026-07-16 11:05:33 +0300

    6

---
+++ ارشيف/5.py
@@ -0,0 +1,4 @@
+time=  int (input("enter the duration in seconds \n "))
+minutes= time // 60
+hours= time // 3600
+print(f" the duration is  {hours} hour {minutes} minute {time % 60}  seconds   ")
---
+++ ارشيف/6.py
@@ -0,0 +1,8 @@
+print(" welcome to app")
+age=input("how old are you \n")
+mo =int (age)
+if  mo >= 12 :
+     print(' yes')
+else: 
+     print('dont') 
+   
