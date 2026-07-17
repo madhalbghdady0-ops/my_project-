f0d5162a8644e7705819d1d8b7a309392054d13e
Moaz <madhalbghdady0@gmail.com>
2026-07-17 21:25:54 +0300

    7

---
+++ ارشيف/7.py
@@ -0,0 +1,10 @@
+print("welcome")
+mo=float(input("Enter your grades? \n"))
+if mo >= 90 :
+    print("Your grade is good")
+elif mo>= 75:
+    print("Your grade is acceptable")
+elif mo >= 50:
+    print("Your score is low")
+else:
+    print("The number is not listed in this category")
