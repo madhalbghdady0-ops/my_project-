9688061fdd5f1df352e3fd710df3c5cfb03f2b7c
Moaz <madhalbghdady0@gmail.com>
2026-08-17 23:02:46 +0300

    44

---
+++ porojects finished/25.py
@@ -0,0 +1,6 @@
+print('welcome to the rectangle area calculation program.,😍 \n')
+length=float(input('Enter the length of the rectangle : '))
+width=float(input('Enter the rectangle is width : '))
+the_spase=length*width
+print(f"the spase ={the_spase}")
+print('thank you for using our program.❤️')
